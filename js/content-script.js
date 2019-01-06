1f1a8e9fb416ad749db3ea1dbad1285154105c9d
Panfen <2809956575@qq.com>
2019-01-06 17:00:46 +0800

    first

---
+++ js/content-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// CSDN 
+	$('aside').hide();  //侧边栏
+	$('.meau-gotop-box').hide();  //
+	$('.pulllog-box').hide();
+	$('.bdsharebuttonbox _360_interactive').hide();
+	document.getElementById("btn-readmore").click();  // 自动点击“阅读更多”
+  console.log('已自动关闭无关项');
+});
